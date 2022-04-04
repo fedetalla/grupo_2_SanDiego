cd3d7c04dc9a4b868626baa6f6b63ab55a8937e7
Martin-Salomon <martinsalomon11@gmail.com>
2022-04-04 11:30:38 -0300

    actualizacion de archivos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = ("express");
-const path = ("path");
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
 const publicPath = path.resolve(__dirname, './public'); 
 app.use(express.static(publicPath));
@@ -13,3 +13,4 @@ app.listen(3080, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+
