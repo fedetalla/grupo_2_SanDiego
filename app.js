8221e18d1b0b58f239773a1406fee8e7407c27e2
fedetalla <fede.talla@hotmail.com>
2022-04-14 17:54:51 -0300

    agregue el login

--- app.js
+++ app.js
@@ -26,3 +26,7 @@ app.get('/productDetail',(req,res)=>{
 app.get('/register',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/register.html'));
 })
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+})
