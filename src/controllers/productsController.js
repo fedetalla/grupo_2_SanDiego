3bfc42da4646ba65fa2749879e8f048476f9d927
MartinSalomon <Martin-Salomon>
2022-05-13 19:52:30 -0300

    update

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../data/dataBase.json');
+const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const productsController = {
