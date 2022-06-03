a393ff17b478464c828d83d3aa656fb3f29afd02
LucasMaquiavelo <lucasmaquiavelo2@gmail.com>
2022-06-02 21:16:03 -0300

    aplicacion express-validator

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
 const path = require("path");
+
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const usersController = {
 
@@ -8,7 +12,12 @@ const usersController = {
     register: (req, res) => {
         return res.render("register")
     },
-
+    processRegister:(req, res) => {
+        return res.send("Se procesó tu formulario con éxito")
+    },
+    profile: (req,res)=>{
+        return res.render("userProfile", {users})
+    }
 }
 
 module.exports = usersController;
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,11 +1,49 @@
 const express = require("express");
-
+const multer = require('multer');
 const router = express.Router();
 
 const usersController = require("../controllers/usersController.js")
 
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null, './public/img/users');
+    },
+    filename: (req,file,cb)=>{
+        cb(null, file.fieldname + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const uploadFile = multer({storage})
+
+const { body } = require('express-validator')
+
+const validations = [
+    body('fullName').notEmpty().withMessage('Tienes que escribir un nombre'),
+    body('category').notEmpty().withMessage('Tienes que escribir un apellido'),
+    body('email').notEmpty().withMessage('Tienes que escribir un correo electrónico válido'),
+    body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
+    body('image').custom((value, {req}) => {
+        let file = req.file;
+        let extensionsAccepted = [ '.png', '.jpg' , '.gif' ];
+        
+        if(!file){
+            throw new Error('Tienes que subir una imagen')
+        }else{
+        let fileExtension = path.extname(file.originalname)
+        if(!extensionsAccepted.includes(fileExtension)){
+            throw new Error('Las extensiones permitidas son: ".png", ".jpg" , ".gif" ');
+        }
+    }
+        return true
+    })
+]
+
 router.get("/login", usersController.login);
 router.get("/register", usersController.register);
+router.post("/register", uploadFile.single('image'), validations , usersController.processRegister);
+
+
+router.get("/profile", usersController.profile);
 
 
 module.exports = router ;
